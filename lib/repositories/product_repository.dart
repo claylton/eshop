444d081890f9aa7772d62fcc52a4cfd16c0c3102
Claylton Santos <clayltonsp@hotmail.com>
2024-09-01 10:27:55 -0300

    add product repository

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -1,4 +1,32 @@
 
+import 'package:dio/dio.dart';
+import 'package:shopping_bloc/models/product_details_model.dart';
+import 'package:shopping_bloc/models/product_list_item_model.dart';
+import 'package:shopping_bloc/settings/settings.dart';
+
 class ProductRepository {
-  
+  Future<List<ProductListItemModel>> getAll() async {
+    String url = "${Settings.apiUrl}/v1/products";
+    Response response = await Dio().get(url);
+
+    return (response.data as List)
+      .map((course) => ProductListItemModel.fromJson(course))
+      .toList();
+  }
+
+  Future<List<ProductListItemModel>> getByCategory(String category) async {
+    String url = "${Settings.apiUrl}/v1/categories/$category/products";
+    Response response = await Dio().get(url);
+    
+    return (response.data as List)
+      .map((course) => ProductListItemModel.fromJson(course))
+      .toList();
+  }
+
+  Future<ProductDetailsModel> get(String tag) async {
+    String url = "${Settings.apiUrl}/v1/products/$tag";
+    Response response = await Dio().get(url);
+    
+    return ProductDetailsModel.fromJson(response.data);
+  }
 }
--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,7 +1,7 @@
 import 'package:shopping_bloc/models/user_model.dart';
 
 class Settings {
-  static String apiUrl = "https://balta-eshop.azurewebsites.net/";
+  static String apiUrl = "https://balta-eshop.azurewebsites.net";
   static String theme = "light";
   static UserModel? user;
 }
